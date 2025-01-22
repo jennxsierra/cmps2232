08796cfa37bbff5838fd286938144d35ad27dd31
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2025-01-21 20:27:17 -0600

    Completed activity 9.3

---
+++ textbook/ch09-03-class-scope/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Count {
+public:
+    void setX(int value) {
+        x = value;
+    }
+
+    void print() const {
+        std::cout << x << std::endl;
+    }
+
+private:
+    int x;
+};
+
+int main() {
+
+    Count counter{};
+    Count *counterPtr = &counter;
+    Count &counterRef = counter;
+
+    std::cout << "Set x to 1 and print using the object name: ";
+    counter.setX(1);
+    counter.print();
+
+    std::cout << "Set x to 2 and print using a reference to an object: ";
+    counterRef.setX(2);
+    counterRef.print();
+
+    std::cout << "Set x to 3 and print using a pointer to an object: ";
+    counterPtr->setX(3);
+    counterPtr->print();
+
+    return 0;
+}
