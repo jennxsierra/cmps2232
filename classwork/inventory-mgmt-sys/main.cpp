84064e197b3fbac8e54482baf95c7965980ad6e2
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2025-01-25 22:55:08 -0600

    Update main

--- classwork/inventory-mgmt-sys/main.cpp
+++ classwork/inventory-mgmt-sys/main.cpp
@@ -2,11 +2,6 @@
 
 int main() {
     std::cout << "Welcome to the Inventory Management System!" << std::endl;
-    if (1 != 0) {
-        std::cout << "This is a test" << std::endl;
-    }
-
-    std::cout << "Goodbye!" << std::endl;
 
     return 0;
 }
